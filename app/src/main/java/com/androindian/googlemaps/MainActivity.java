55e7f65c526fbeb88aacf47bc78dc39232bd77a0
Raj <jangam.rajasekar@gmail.com>
2019-03-25 10:26:14 +0530

    Initial commit

---
+++ app/src/main/java/com/androindian/googlemaps/MainActivity.java
@@ -0,0 +1,46 @@
+package com.androindian.googlemaps;
+
+import android.location.Location;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MainActivity extends AppCompatActivity
+        implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        LatLng hyd = new LatLng(-17.6436436, 78.35353);
+        mMap.addMarker(new MarkerOptions().position(hyd).title("Hyderbad"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(hyd));
+    }
+}
